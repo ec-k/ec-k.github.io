7cda98c71efa50d942a2fb0e5491c2d6f8530db9
EC-K <elxone.19jt@gmail.com>
2025-08-28 00:23:41 +0900

    refactor: define portfolio home

---
+++ src/app/portfolio/PortfolioHome.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function PortfolioHome() {
+  return (
+    <main className="w-full max-w-7xl">
+      <div className="relative h-120 mb-12 w-full overflow-hidden">
+        <Image
+          src="/images/my_t_pose.png"
+          alt="Picture of the author"
+          width={1920}
+          height={1080}
+          style={{ objectFit: "cover", objectPosition: "center" }}
+          priority
+          quality={80}
+          className="absolute inset-0"
+        />
+        <div className="absolute bottom-0 left-0 z-10 p-10 text-white">
+          <p className="text-6xl mb-5">怠惰．</p>
+          <p className="text-xl ml-1">石射 彗 / Ishi Kei</p>
+        </div>
+      </div>
+    </main>
+  );
+}
