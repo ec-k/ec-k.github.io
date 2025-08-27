e2d317a7ec0c579304aa8eec1278c83d3e0198a4
EC-K <elxone.19jt@gmail.com>
2025-08-28 00:21:44 +0900

    art: place links in header instead of footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <div className="font-sans grid-rows-[min-content_1fr_min-content] min-h-screen p-8 pb-20 gap-16 sm:p-20 mx-auto">
       <header className="w-full max-w-7xl">
+        <div className="grid grid-cols-4 content-start gap-4">
+          <p>link</p>
+          <a href="https://www.pixiv.net/users/14838049">Pixiv</a>
+          <a href="https://github.com/ec-k">GitHub</a>
+          <a href="https://note.com/ec_k">note</a>
+        </div>
         <div className="grid grid-cols-3 content-start gap-4">
           <Link href="/">home</Link>
           <Link href="/blog">blog</Link>
@@ -28,14 +34,6 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <footer className="w-full max-w-7xl">
-        <div className="grid grid-cols-4 content-start gap-4">
-          <p>link</p>
-          <a href="https://www.pixiv.net/users/14838049">Pixiv</a>
-          <a href="https://github.com/ec-k">GitHub</a>
-          <a href="https://note.com/ec_k">note</a>
-        </div>
-      </footer>
     </div>
   );
 }
