b57a206d52d6dba990dc21332bdc98cb38075f1e
EC-K <elxone.19jt@gmail.com>
2025-08-29 00:13:03 +0900

    feat: add social links in ABOUT

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,25 @@
-export default function AboutPage() {
+import ProfileIcon from "../components/ProfileIcon";
+import SocialLinks from "../components/SocialLinks";
+
+export default function About() {
   return (
-    <div className="container mx-auto p-4">
-      <h1>ABOUT</h1>
-      <p>準備中</p>
+    <div className="h-screen w-full mt-20 gap-4">
+      <h1 className="flex justify-center text-4xl">ABOUT</h1>
+      <div className="w-full flex justify-center items-center">
+        <div className="flex items-center">
+          <ProfileIcon src={"/images/author_image.jpg"} size={128} />
+        </div>
+        <div className="gap-4">
+          <div>
+            <p className="text-sm">Ishi Kei</p>
+            <p className="text-lg">石射 彗</p>
+          </div>
+          <div>
+            <p>怠惰．</p>
+          </div>
+          <SocialLinks />
+        </div>
+      </div>
     </div>
   );
 }
