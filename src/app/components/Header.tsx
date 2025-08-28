e958e256c550a08787e3d7f0388a90da8c0c8326
EC-K <elxone.19jt@gmail.com>
2025-08-28 14:07:15 +0900

    art: place icons in link

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faPixiv } from "@fortawesome/free-brands-svg-icons";
 
 export default function Header() {
   return (
@@ -9,9 +12,28 @@ export default function Header() {
           <Link href="/blog">blog</Link>
         </div>
         <div className="flex justify-end gap-4">
-          <a href="https://www.pixiv.net/users/14838049">Pixiv</a>
-          <a href="https://github.com/ec-k">GitHub</a>
-          <a href="https://note.com/ec_k">note</a>
+          <a href="https://www.pixiv.net/users/14838049">
+            <FontAwesomeIcon
+              icon={faPixiv}
+              className="h-[32px]"
+              color="white"
+            />
+          </a>
+          <a href="https://github.com/ec-k">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="h-[32px]"
+              color="white"
+            />
+          </a>
+          <a href="https://note.com/ec_k">
+            <Image
+              src="/images/note_icon.svg"
+              alt="note icon"
+              width={32}
+              height={32}
+            />
+          </a>
         </div>
       </div>
     </header>
