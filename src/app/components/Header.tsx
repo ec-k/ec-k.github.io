b2913278d8ceb972e524fd4da66aecce5a81db86
EC-K <elxone.19jt@gmail.com>
2025-08-28 18:25:27 +0900

    refactor: remove redantant css properties

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full sticky top-0 z-50 h-16 text-white">
+    <header className="w-full sticky top-0 z-50 h-16 text-white relative">
       <div className="absolute inset-0 bg-black opacity-75 z-0"></div>
       <div className="w-full h-full z-10 relative flex items-center px-20">
-        <div className="w-full h-full flex justify-start gap-6 items-center">
+        <div className="flex flex-grow justify-start gap-6 items-center">
           <Image
             src="/icon-192x192.png"
             alt="Icon of author"
@@ -15,7 +15,7 @@ export default function Header() {
           />
           <p className="text-2xl ">EC-K</p>
         </div>
-        <div className="w-full h-full flex justify-end gap-5 text-lg items-center">
+        <div className="flex flex-grow justify-end gap-5 text-lg items-center">
           <Link href="/">HOME</Link>
           <Link href="/about">ABOUT</Link>
           <Link href="/works">WORKS</Link>
