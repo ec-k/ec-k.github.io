82209efcf62bf28814ad1c374844dec54e0aebc1
EC-K <elxone.19jt@gmail.com>
2025-08-28 17:23:08 +0900

    refactor: change a class name

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function PortfolioHome() {
+export default function Hero() {
   return (
     <main className="w-full">
       <div className="relative h-screen w-full overflow-hidden">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import PortfolioHome from "./components/PortfolioHome";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="font-sans grid-rows-[min-content_1fr_min-content] min-h-screen p-8 gap-16 sm:p-20 mx-auto">
-      <PortfolioHome />
+      <Hero />
     </div>
   );
 }
