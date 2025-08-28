dbe3fc26386bb09695dde568f76fae13849f8a2b
EC-K <elxone.19jt@gmail.com>
2025-08-28 15:00:28 +0900

    art: place author icon in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full h-16 flex items-center">
-      <div className="w-full gap-5 ml-20">
+    <header className="w-full h-16 flex items-center bg-black">
+      <div className="w-full flex justify-start gap-6 ml-20 items-center">
+        <Image
+          src="/icon-192x192.png"
+          alt="Icon of author"
+          width={46}
+          height={46}
+        />
         <p className="text-2xl ">EC-K</p>
       </div>
       <div className="w-full flex justify-end gap-5 text-lg mr-20">
