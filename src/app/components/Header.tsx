c31fff0d208a4e0de1439f75e1c966c38fd5e1e1
EC-K <elxone.19jt@gmail.com>
2025-08-28 18:09:48 +0900

    art: revise header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,21 +3,24 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full h-16 flex items-center bg-black">
-      <div className="w-full flex justify-start gap-6 ml-20 items-center">
-        <Image
-          src="/icon-192x192.png"
-          alt="Icon of author"
-          width={46}
-          height={46}
-        />
-        <p className="text-2xl ">EC-K</p>
-      </div>
-      <div className="w-full flex justify-end gap-5 text-lg mr-20">
-        <Link href="/">HOME</Link>
-        <Link href="/about">ABOUT</Link>
-        <Link href="/works">WORKS</Link>
-        <Link href="/blog">BLOG</Link>
+    <header className="w-full sticky top-0 z-50 h-16 text-white">
+      <div className="absolute inset-0 bg-black opacity-75 z-0"></div>
+      <div className="w-full h-full z-10 relative flex items-center px-20">
+        <div className="w-full h-full flex justify-start gap-6 items-center">
+          <Image
+            src="/icon-192x192.png"
+            alt="Icon of author"
+            width={46}
+            height={46}
+          />
+          <p className="text-2xl ">EC-K</p>
+        </div>
+        <div className="w-full h-full flex justify-end gap-5 text-lg items-center">
+          <Link href="/">HOME</Link>
+          <Link href="/about">ABOUT</Link>
+          <Link href="/works">WORKS</Link>
+          <Link href="/blog">BLOG</Link>
+        </div>
       </div>
     </header>
   );
