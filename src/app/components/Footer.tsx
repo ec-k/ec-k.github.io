6c13c1e82416b8f4b33783bc8b5f8a6fadecb9e7
EC-K <elxone.19jt@gmail.com>
2025-08-29 00:12:13 +0900

    refactor: decouple social links from footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,26 +1,9 @@
-import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faPixiv } from "@fortawesome/free-brands-svg-icons";
+import SocialLinks from "./SocialLinks";
 
 export default function Footer() {
   return (
     <footer className="w-full">
-      <div className="flex justify-end gap-5 pr-10">
-        <a href="https://www.pixiv.net/users/14838049">
-          <FontAwesomeIcon icon={faPixiv} className="h-[32px]" color="white" />
-        </a>
-        <a href="https://github.com/ec-k">
-          <FontAwesomeIcon icon={faGithub} className="h-[32px]" color="white" />
-        </a>
-        <a href="https://note.com/ec_k">
-          <Image
-            src="/images/note_icon.svg"
-            alt="note icon"
-            width={32}
-            height={32}
-          />
-        </a>
-      </div>
+      <SocialLinks />
     </footer>
   );
 }
