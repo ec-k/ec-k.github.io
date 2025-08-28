7c9deee3461a94f11a926591d5cd7957e1b3fe83
EC-K <elxone.19jt@gmail.com>
2025-08-28 12:59:11 +0900

    refactor: decouple  Header as a new single file

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full max-w-7xl">
+      <div className="flex justify-start gap-4">
+        <Link href="/">home</Link>
+        <Link href="/blog">blog</Link>
+      </div>
+      <div className="flex justify-end gap-4">
+        <a href="https://www.pixiv.net/users/14838049">Pixiv</a>
+        <a href="https://github.com/ec-k">GitHub</a>
+        <a href="https://note.com/ec_k">note</a>
+      </div>
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import Header from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,17 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="w-full max-w-7xl">
-          <div className="flex justify-start gap-4">
-            <Link href="/">home</Link>
-            <Link href="/blog">blog</Link>
-          </div>
-          <div className="flex justify-end gap-4">
-            <a href="https://www.pixiv.net/users/14838049">Pixiv</a>
-            <a href="https://github.com/ec-k">GitHub</a>
-            <a href="https://note.com/ec_k">note</a>
-          </div>
-        </header>
+        <Header />
         {children}
       </body>
     </html>
