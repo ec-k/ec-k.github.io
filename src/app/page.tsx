f48448d3b1fa7840bdf478013597304c33021fae
EC-K <elxone.19jt@gmail.com>
2025-08-28 15:47:44 +0900

    wip(art): hastle to fill in screen hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PortfolioHome from "./components/PortfolioHome";
 
 export default function Home() {
   return (
-    <div className="font-sans grid-rows-[min-content_1fr_min-content] min-h-screen p-8 pb-20 gap-16 sm:p-20 mx-auto">
+    <div className="font-sans grid-rows-[min-content_1fr_min-content] min-h-screen p-8 gap-16 sm:p-20 mx-auto">
       <PortfolioHome />
     </div>
   );
