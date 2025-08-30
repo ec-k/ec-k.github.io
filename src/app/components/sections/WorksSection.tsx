4821481ebb1fb591e2decd1f536ec59187f11c05
EC-K <elxone.19jt@gmail.com>
2025-08-30 17:18:57 +0900

    refactor: replace cards with ContentCard

--- src/app/components/sections/WorksSection.tsx
+++ src/app/components/sections/WorksSection.tsx
@@ -1,21 +1,26 @@
+import ContentCard from "../ContentCard";
+
 export default function WorksSection() {
   return (
     <section className="h-screen w-full pt-20" id="works-section">
       <div className="h-full px-20 grid grid-rows-[1fr_2fr] gap-10">
         <h1 className="flex justify-center items-center text-4xl">WORKS</h1>
-        <div className="h-full w-full flex justify-center items-center">
-          <div className="h-60 w-60 flex flex-col justify-center">
-            <h2 className="">Graphics</h2>
-            <div className="h-40 w-40 bg-gray-400"></div>
-          </div>
-          <div className="h-60 w-60 flex flex-col justify-center">
-            <h2 className="">Software</h2>
-            <div className="h-40 w-40 bg-gray-500"></div>
-          </div>
-          <div className="h-60 w-60 flex flex-col justify-center">
-            <h2 className="">Something</h2>
-            <div className="h-40 w-40 bg-gray-600"></div>
-          </div>
+        <div className="h-full w-full flex justify-center items-center gap-4">
+          <ContentCard
+            size={40}
+            categoryName="Graphics"
+            imageSrc="/icon-512x512.png"
+          />
+          <ContentCard
+            size={40}
+            categoryName="Software"
+            imageSrc="/icon-512x512.png"
+          />
+          <ContentCard
+            size={40}
+            categoryName="Something"
+            imageSrc="/icon-512x512.png"
+          />
         </div>
       </div>
     </section>
