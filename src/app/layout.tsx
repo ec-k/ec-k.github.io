387817aca2b4f362143dda2a44a16350e3712e31
EC-K <elxone.19jt@gmail.com>
2025-08-30 16:31:57 +0900

    art: change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,29 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Lato, Zen_Kaku_Gothic_New, Kiwi_Maru } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+
+const lato = Lato({
+  weight: "300",
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-lato",
+});
+
+const zenKaku = Zen_Kaku_Gothic_New({
+  weight: "300",
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-zen-kaku-gothic-new",
+});
+
+const kiwiMaru = Kiwi_Maru({
+  weight: ["300", "400"],
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-kiwi-maru",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +46,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="jp">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lato.variable}  ${zenKaku.variable}  ${kiwiMaru.variable} antialiased`}
       >
         <div className="font-sans grid grid-rows-[1fr_min-content] min-h-screen">
           <Header />
