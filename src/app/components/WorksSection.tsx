076aba16564b0f97a326007789853b0adb885d4e
EC-K <elxone.19jt@gmail.com>
2025-08-30 14:06:22 +0900

    art: define layout with placeholder

--- src/app/components/WorksSection.tsx
+++ src/app/components/WorksSection.tsx
@@ -1,3 +1,23 @@
 export default function WorksSection() {
-  return <section id="works-section"></section>;
+  return (
+    <section className="h-screen w-full pt-20" id="works-section">
+      <div className="h-full px-20 pt-30 grid grid-rows-1-2 gap-10">
+        <h1 className="flex justify-center text-4xl">WORKS</h1>
+        <div className="h-full w-full flex justify-center items-center">
+          <div className="h-60 w-60 flex flex-col items-center justify-center">
+            <h2 className="text-center">Graphics</h2>
+            <div className="h-40 w-40 bg-gray-400"></div>
+          </div>
+          <div className="h-60 w-60 flex flex-col items-center justify-center">
+            <h2 className="text-center">Software</h2>
+            <div className="h-40 w-40 bg-gray-500"></div>
+          </div>
+          <div className="h-60 w-60 flex flex-col items-center justify-center">
+            <h2 className="text-center">Something</h2>
+            <div className="h-40 w-40 bg-gray-600"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
